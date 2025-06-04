7137d0ece8b4ffb94571528a2276fbac763e70e0
coding star <pradipkakade321@gmail.com>
2025-06-04 20:48:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mydev-project-terraform-sample-batch-30"
-    key = "main"
+    key = "eks/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
     use_lockfile  = true
